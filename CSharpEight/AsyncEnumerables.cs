aeddce3c20be4bd71cbb9a5714ba8fc6a4a68bd8
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-10-22 16:35:54 +0300

    Add demo about async enumerables

---
+++ CSharpEight/AsyncEnumerables.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Starting with C# 8.0, you can create and consume streams asynchronously. 
+ * A method that returns an asynchronous stream has three properties:
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharpEight
+{
+    public class AsyncEnumerables
+    {
+        public static async Task Demo()
+        {
+            //foreach (var number in GenerateSequence())
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            await foreach (var number in GenerateSequenceAsync())
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static IEnumerable<int> GenerateSequence()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                //await Task.Delay(100); // Some long running operation. Can't use 'await' here
+                yield return i;
+            }
+        }
+
+        public static async IAsyncEnumerable<int> GenerateSequenceAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+    }
+}
