c4c446320af7fa8bed0d25d4d3b1a9e6701536cd
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-10-22 17:56:16 +0300

    Add demo about default interface member

---
+++ CSharpEight/DefaultInterfaceMethods.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * You can now add members to interfaces and provide an implementation for those members.
+ * Often use-case: post factum adding new members to exicting API
+ */
+
+using System;
+
+namespace CSharpEight
+{
+    public interface IHuman
+    {
+        string Name { get; set; }
+
+        void SayHello()
+        {
+            Console.WriteLine($"Hello, I am {Name}");
+        }
+    }
+
+    public interface IFriendlyHuman : IHuman
+    {
+        void IHuman.SayHello()
+        //new void SayHello()
+        {
+            Console.WriteLine($"Greeting, my name is {Name}");
+        }
+    }
+
+    public class Human : IHuman
+    {
+        public string Name { get; set; }
+    }
+
+    public class DefaultInterfaceMethods
+    {
+        public static void Demo()
+        {
+            var human = new Human() { Name = "John" };
+            //human.SayHello(); // Will not compile
+
+            IHuman human1 = new Human() { Name = "John" };
+            human1.SayHello();
+
+            ((IHuman)human).SayHello();
+            
+
+            //var h = new NewHuman();
+            //(h as IHuman).SayHello();
+        }
+    }
+    //public interface IHumbleHuman : IHuman
+    //{
+    //    void IHuman.SayHello()
+    //    {
+    //        Console.WriteLine($"Hello");
+    //    }
+    //}
+    //public class NewHuman : IFriendlyHuman, IHumbleHuman
+    //{
+    //    public string Name { get; set; }
+    //}
+}
