c3f142be7c1455ae892da6b265390d89e00c1661
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-10-22 14:47:23 +0300

    Add demo about stackalloc in nested expressions

---
+++ CSharpEight/StackallocInNestedExpressions.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Starting with C# 8.0, if the result of a stackalloc expression 
+ * is of the System.Span<T> or System.ReadOnlySpan<T> type, you 
+ * can use the stackalloc expression in other expressions:
+ */
+
+using System;
+
+namespace CSharpEight
+{
+    public class StackallocInNestedExpressions
+    {
+        static public void Demo()
+        {
+            Span<int> numbers = stackalloc[] { 1, 2, 3, 4, 5, 6 };
+            var ind = numbers.IndexOfAny(stackalloc[] { 2, 4, 6, 8 });
+
+            //Span<int> span = stackalloc[] { 2, 4, 6, 8 };
+            //var ind = numbers.IndexOfAny(span);
+
+            Console.WriteLine(ind);
+        }
+    }
+}
