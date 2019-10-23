f44787995933a6f23d2af33fcae59bc19786faa0
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-10-23 12:11:29 +0300

    Separate enhancement of interpolated verbatim strings feature

--- EnhancementOfInterpolatedVerbatimStrings/Program.cs
+++ EnhancementOfInterpolatedVerbatimStrings/Program.cs
@@ -2,14 +2,14 @@
  *  Both $@"..." and @$"..." are valid interpolated verbatim strings.
  *  In earlier C# versions, the $ token must appear before the @ token.
  */
+ 
+ using System;
 
-using System;
-
-namespace CSharpEight
+namespace EnhancementOfInterpolatedVerbatimStrings
 {
-    public class EnhancementOfInterpolatedVerbatimStrings
+    class Program
     {
-        public static void Demo()
+        static void Main(string[] args)
         {
             var dateTime = DateTime.Now.ToString();
             Console.WriteLine(@"C:\Windows\System32\cmd.exe " + dateTime);
